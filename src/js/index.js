3554b9a2bd4b09c085689e9b42967ad7a43746f8
Tailorjr <82546550+Tailorjr@users.noreply.github.com>
2023-03-11 23:19:07 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+/* quando clicarmos no botão, trocamos a imagem de fundo */
+
+const botoesCarrossel = document.querySelectorAll(".botao");
+const imagens = document.querySelectorAll(".imagem");
+
+botoesCarrossel.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    const botaoSelecionado = document.querySelector(".selecionado");
+
+    botaoSelecionado.classList.remove("selecionado");
+
+    botao.classList.add("selecionado");
+
+    const imagemAtiva = document.querySelector(".ativa");
+
+    imagemAtiva.classList.remove("ativa");
+
+    imagens[indice].classList.add("ativa");
+  });
+});
